616803195b9456c971adf7d4fead4dcbe2f26542
Tracklove <Tracklove00@Gmail.com>
2023-07-31 18:11:40 +0330

    Add files via upload

---
+++ Tass.py
@@ -0,0 +1,26 @@
+#Tass Bendaz
+while True:
+    
+    import random as rn
+    Tass=str(input("Tasss Bendaz :)) >> Type 'Enter' "))
+    if Tass=="Enter":
+        tass= rn.randint(1,6)
+        print(tass)
+        if tass==1:
+            result=1
+            break
+        if tass==2:
+            result=2
+            break
+        if tass==3:
+            result=3
+            break
+        if tass==4:
+            result=4
+            break
+        if tass==5:
+            result=5
+            break
+        if tass==6:
+            result="6 // Tabrik Dobare Tass Bendaz"
+            print(result)
